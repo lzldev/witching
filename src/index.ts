ceea76f3a0be1024295094f76953826e0781666b
lucas <francisco.lucas0507@gmail.com>
2023-11-19 07:29:32 -0300

    Command Execution and Ignore Option

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { version } from '../package.json'
-import { Command, Option } from '@commander-js/extra-typings'
+import { Command } from '@commander-js/extra-typings'
 import { writeRed } from './utils'
 import {
-  BackendOption,
   EventOption,
+  IgnoreOption,
   InteractiveOption,
   SilentOption,
 } from './options'
+import { execa } from 'execa'
+import parcel from '@parcel/watcher'
+import chalk from 'chalk'
+import picomatch from 'picomatch'
 
 const l = console.log
 
@@ -17,22 +21,71 @@ const app = new Command()
   .configureOutput({
     outputError: writeRed,
   })
-  .argument('<watch-dir>', 'dir to be watched')
-  .argument('<command>', 'Command on file change')
+  .argument('<WATCH-DIR>', 'dir to be watched')
+  .argument(
+    '<COMMAND>',
+    'Command on file change\n % is going to be replaced with the file name',
+  )
+  .addOption(IgnoreOption)
   .addOption(EventOption)
-  .addOption(BackendOption)
   .addOption(SilentOption)
   .addOption(InteractiveOption)
 
 app.parse()
 
-function main() {
+async function main() {
   const [dir, command] = app.args
   const options = app.opts()
 
-  l(`Watching Dir ${dir}`)
-  l(`Command: ${command}`)
-  l(`Options :\n`, options)
+  if (!dir || !command) {
+    l('Error:', chalk.red('Invalid arguments'))
+    return
+  }
+
+  l(
+    `
+  Watching Dir ${chalk.magentaBright(dir)}
+  Command: ${chalk.green(command)}
+  Options :`,
+    options,
+    '\n',
+  )
+
+  let matcher: picomatch.Matcher
+
+  if (options.i) {
+    matcher = picomatch(options.i)
+    l(chalk.green('ignore:') + ` [${options.i}]\n`)
+  }
+
+  await parcel.subscribe(dir, async (err, events) => {
+    const evt = (...txt: any[]) => chalk.bgCyan(chalk.whiteBright(...txt))
+
+    for (const ev of events) {
+      if (matcher && matcher(ev.path)) {
+        l(`${chalk.cyan(' FILE: ')}${ev.path}${chalk.redBright(' IGNORED')}`)
+        continue
+      }
+
+      const cmd_str = command.replace('%', ev.path)
+
+      l(
+        `EVENT:${evt(` ${ev.type.toUpperCase()} `)}\n`,
+        `FILE ${chalk.magenta(ev.path)}\n`,
+        `RUNNING ${chalk.greenBright(cmd_str)}\n`,
+      )
+
+      const cmd = await execa(cmd_str, {
+        stdin: process.stdin,
+        stdout: process.stdout,
+        stderr: process.stderr,
+      }).catch((err) => {
+        l(`Error Running: ${chalk.green(cmd_str)}\n`, chalk.red(err))
+      })
+
+      l(`${chalk.green(`DONE`)} ✅\n`)
+    }
+  })
 }
 
-main()
+await main()
--- src/options.ts
+++ src/options.ts
@@ -1,28 +1,48 @@
 import { Option } from '@commander-js/extra-typings'
 
-const EventChoices = ['tbd', 'tbd1', 'tbd2'] as const
-
-export type EventChoices = typeof EventChoices
-export type EventChoice = (typeof EventChoices)[number]
-
-const EventOption = new Option('-e <events>,--events <events>', '').choices(
-  EventChoices,
-)
-
+/* 
+@parcel/watcher Parcel BackendType
+  export type BackendType =
+    | 'fs-events'
+    | 'watchman'
+    | 'inotify'
+    | 'windows'
+    | 'brute-force';
+TODO: i should probably remove this . at least hide it from the --help
 const BackendChoices = ['tbd', 'tbd1', 'tbd2'] as const
-
 export type BackendChoices = typeof BackendChoices
 export type BackendChoice = (typeof BackendChoices)[number]
-
 const BackendOption = new Option(
   '-b <backend> ,--backend <backend>',
   'parcel watcher backend',
 ).choices(BackendChoices)
+  
 
-const InteractiveOption = new Option('-i ,--interactive')
-const SilentOption = new Option(
-  '-s ,--silent',
-  'runs commands without displaying output',
+*/
+
+/* 
+@parcel/watcher EventType
+  EventType :  'create' | 'update' | 'delete';
+*/
+export const EventChoices = ['create', 'update', 'delete'] as const
+
+export type EventChoices = typeof EventChoices
+export type EventChoice = (typeof EventChoices)[number]
+
+const EventOption = new Option('-e <EVENTS>,--events <EVENTS>', '').choices(
+  EventChoices,
 )
 
-export { EventOption, BackendOption, InteractiveOption, SilentOption }
+const InteractiveOption = new Option('--interactive')
+
+const SilentOption = new Option(
+  '-s ,--silent',
+  'Runs commands without displaying output',
+)
+
+const IgnoreOption = new Option(
+  '-i <GLOB>,--ignore <GLOB>',
+  'every file matching the pattern will be ignored',
+)
+
+export { EventOption, InteractiveOption, SilentOption, IgnoreOption }
